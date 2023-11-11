80b7b7da0a31722304a8c6ee38068809b2368d62
Rolando Hernandez <rolandohernandezleal@gmail.com>
2023-11-11 08:41:06 -0300

    add some stuff

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 (function () {
     // Menu
-    $('.menu-item').on('click', function () {
-        $('.menu-item').removeClass('is-active');
+    $('.collection-menu-item').on('click', function () {
+        $('.collection-menu-item').removeClass('is-active');
         $(this).addClass('is-active');
         $('.collection').removeClass('is-active');
         $($(this).data('collection')).addClass('is-active');
